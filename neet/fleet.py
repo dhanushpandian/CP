0dab1e0996d80c9e552e3c53c03d181f20b74e43
Dash <dhanushdpandian@gmail.com>
2023-08-21 23:32:06 +0530

    fleet

---
+++ neet/fleet.py
@@ -0,0 +1,11 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        fleets = []
+        for p, s in sorted(zip(position, speed), reverse=True):
+            reach = (target - p) / s 
+            if not fleets: 
+                fleets.append(reach)
+            elif reach > fleets[-1]:
+                fleets.append(reach)
+            
+        return len(fleets)  
