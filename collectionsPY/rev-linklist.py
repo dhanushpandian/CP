938a56e25506dcf3105b037c6ad07e979de767c1
Dash <dhanushdpandian@gmail.com>
2024-01-22 20:21:42 +0530

    reverse using recursion

--- collectionsPY/rev-linklist.py
+++ collectionsPY/rev-linklist.py
@@ -1,6 +1,15 @@
-import collections
-from collections import deque
-d=deque("hello")
-print(d)
-d.append('3')
-print(d)
+#Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev=None
+        curr=head
+        while(curr):
+            nxt=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nxt
+        return prev
