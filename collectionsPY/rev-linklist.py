04137942ec80b6697d44f36769476ae17861bd5a
Dash <dhanushdpandian@gmail.com>
2024-01-29 23:28:05 +0530

    reverse of ll

--- collectionsPY/rev-linklist.py
+++ collectionsPY/rev-linklist.py
@@ -13,3 +13,16 @@ class Solution:
             prev=curr
             curr=nxt
         return prev
+
+#using recursion
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        newHead=head
+        if head.next:
+            newHead=self.reverseList(head.next)
+            head.next.next=head
+        head.next=None
+
+        return newHead
