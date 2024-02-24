e0bfae006f64aa900bb8e237e64a012e8e9f7bf5
Dash <dhanushdpandian@gmail.com>
2024-02-24 21:54:03 +0530

    implementation using floyds

--- neet/duplicateinrange.py
+++ neet/duplicateinrange.py
@@ -1,8 +1,22 @@
-class Solution:
-    def findDuplicate(self, nums) -> int:
-        d={}
-        for i in nums:
-            if i in d:
-                return i
-            else:
-                d[i]=1
+def findDuplicate(self, nums) -> int:
+    d={}
+    for i in nums:
+        if i in d:
+            return i
+        else:
+            d[i]=1
+
+#using linkedlist floyds algo
+def findDuplicate2(self, nums: List[int]) -> int:
+    slow=nums[0]
+    fast=nums[0]
+    while(True):
+        slow=nums[slow]
+        fast=nums[nums[fast]]
+        if slow==fast:
+            break
+    slow2=nums[0]
+    while slow!=slow2:
+        slow=nums[slow]
+        slow2=nums[slow2]
+    return slow2
