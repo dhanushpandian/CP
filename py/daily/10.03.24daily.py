42b35184ad8b91bfbcedd14f0c895f66b46715da
Dash <dhanushdpandian@gmail.com>
2024-03-10 13:50:27 +0530

    daily

---
+++ py/daily/10.03.24daily.py
@@ -0,0 +1,22 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        a=set(nums1)
+        b=set(nums2)
+        c=[]
+        for i in a:
+            if i in b:
+                c.append(i)
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        a=set(nums1)
+        b=set(nums2)
+        c=[i for i in a if i in b]
+        return c
+    
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return [i for i in set(nums1) if i in set(nums2)]
+        
+        
+        
