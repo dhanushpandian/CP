bfde9f5529db29d26ba0e0bf4cfcec5243661028
Dash <dhanushdpandian@gmail.com>
2024-05-09 01:30:02 +0530

    dict rel rank

--- py/daily/9.05.2024daily.py
+++ py/daily/9.05.2024daily.py
@@ -1,5 +1,31 @@
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
+        N = len(score)
+       
+        # Save the index of each athelete
+        score_to_index = {}
+        for i in range(N):
+            score_to_index[score[i]] = i
+
+        # Sort score in descending order
+        score.sort(reverse = True)
+
+        # Assign ranks to athletes
+        rank = [" "] * N
+        for i in range(N):
+            if i == 0:
+                rank[score_to_index[score[i]]] = "Gold Medal"
+            elif i == 1:
+                rank[score_to_index[score[i]]] = "Silver Medal"
+            elif i == 2:
+                rank[score_to_index[score[i]]] = "Bronze Medal"
+            else:
+                rank[score_to_index[score[i]]] = str(i + 1)
+
+        return rank
+    
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
         ranked = sorted(score, reverse=True)  # Sort the scores in descending order
         medals = ["Gold Medal", "Silver Medal", "Bronze Medal"] + [str(i + 1) for i in range(3, len(score))]
         rank_dict = {score: medal for score, medal in zip(ranked, medals)}  # Create a dictionary to store ranks
