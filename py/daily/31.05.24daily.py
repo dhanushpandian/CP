a84d9b3302442de464869050fa70ce65615e8157
Dash <dhanushdpandian@gmail.com>
2024-05-31 13:00:34 +0530

    daily bit mani

--- py/daily/31.05.24daily.py
+++ py/daily/31.05.24daily.py
@@ -3,4 +3,40 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         d={i:nums.count(i) for i in nums}
         return [i for i,j in d.items() if j==1]
+
+#list Apporach
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        ans = set()
+        for n in nums:
+            if n in ans:
+                ans.remove(n)
+            else:
+                ans.add(n)
+        return list(ans)
     
+#slightly better Aprroch 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        d={}
+        for i in nums:
+            if i in d:
+                del d[i]
+            else:
+                d[i]=1
+        return list(d.keys())
+
+#Bit Manipulation       
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+        for n in nums:
+            xor ^= n
+        mask = xor & -xor
+        a = b = 0
+        for n in nums:
+            if n & mask:
+                a ^= n
+            else:
+                b ^= n
+        return [a, b]
