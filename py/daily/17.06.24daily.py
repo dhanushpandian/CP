edc7d22a644d82f3cd33f299d05baad08367e20f
Dash <dhanushdpandian@gmail.com>
2024-06-17 11:41:48 +0530

    added 2p approach

--- py/daily/17.06.24daily.py
+++ py/daily/17.06.24daily.py
@@ -7,6 +7,21 @@ class Solution:
                 if i**2 + j**2 == c:
                     return True
         return False
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        # d=set()
+
+        def is_perfect_square(num):
+            sqrt_num = int(num**0.5)
+            return sqrt_num * sqrt_num == num
+
+        for i in range(int(c**0.5)+1):
+            x = c - i*i
+            if is_perfect_square(x):
+                return True    
+            # d.add(x)
+        return False
     
 #memo approach:
 class Solution:
@@ -24,3 +39,16 @@ class Solution:
         return False
 
 #two pointer approach:
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        r=int(sqrt(c))
+        l=0
+        while l <= r  :
+            x =  l**2 + r**2 
+            if  x == c :
+                return True
+            elif x > c:
+                r-=1
+            else :
+                l+=1
+        return False
