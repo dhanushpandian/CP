ee3eef74e58759224e080a372b10758d61a71941
Dash <dhanushdpandian@gmail.com>
2024-07-23 21:17:52 +0530

    lamda used

--- py/daily/23.07.24daily.py
+++ py/daily/23.07.24daily.py
@@ -1,3 +1,15 @@
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        ans=sorted(nums ,key = lambda x: (d[x] , -x) )
+        return ans
+
+
 
 class Solution:
     def frequencySort(self, nums):
