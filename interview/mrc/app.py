159cd516e047065c021cab9c2b5b368dfff81d2d
Dash <dhanushdpandian@gmail.com>
2024-07-28 17:55:05 +0530

    added t/f

--- interview/mrc/app.py
+++ interview/mrc/app.py
@@ -25,18 +25,31 @@ class Quizr(User):
         super().__init__(username, uid, password)
         self.company=company
         print("User "+self.username+" created!")
+
     def addquiz(self,type,domain):
-        Quizr.quizes[domain]=[]
-        if type =="mcq":
-            n=int(input("enter number of questions: "))
-            qns=[]
+        Quizr.quizes[self.uid]={domain:[]}
+        n=int(input("enter number of questions: "))
+        qns=[]
+    
+        if type=="mcq":
             for i in range(n):
                 d={}
-                x=input("enetr question ",i+1, ": ")
-                os=input("eneter options: ")
-                crt=input("emter correct ans : ")
+                x=input("enter question {}: ".format(i+1))
+                os=input("enter options: ")
+                crt=input("enter correct ans: ")
                 d[i+1]=[x,os,crt]
                 qns.append(d)
+                Quizr.quizes[domain]=qns
+        elif type=="T/F" or type=="t/f":
+            for i in range(n):
+                d={}
+                x=input("enter question {}: ".format(i+1))
+                crt=input("enter correct ans: ")
+                d[i+1]=[x,crt]
+                qns.append(d)
+            Quizr.quizes[domain]=qns
+        else:
+            print("Invalid type")
 
 
 
@@ -50,9 +63,10 @@ def participant(uid):
                 print(Quizr.quizes)
             elif x==2:
                 do=input("enetr domain name : ")
-                print(Quizr.quizes[do])
+                op= Quizr.quizes[do] if Quizr.quizes[do] else "No quiz found"
+                print(op)
         elif opt==2:
-            pass
+            print(Parti.scores[uid])
         elif opt==3:
             pass
         elif opt==4:
@@ -68,58 +82,69 @@ def quizzer(uid):
         if opt==2:
             t=input("type: ")
             dom=input("domain: ")
-            Quizr.addquiz(t,dom)        
+            Quizr.addquiz(uid,t,dom)        
 
+def quizapp():
+    while True:
+        print("==========================================")
+        print("Enter Option 1> Login 2> Register")
+        lr=int(input())
+        if lr==1:
+            print("Login as Enter Option 1 or 2 \n 1> participant 2> quizerr")
+            opt=int(input())
+            if opt==1:
+                pid=input("enter userid : ")
+                ppass=input("enter pasword : ")
+                if User.users[pid][1]==ppass:
+                    print("Logged in as "+User.users[pid][0])
+                    participant(pid)
+                else:
+                    print("Invalid Creds")
+            elif opt==2:
+                pid=input("enter userid : ")
+                ppass=input("enter pasword : ")
+                if User.users[pid][1]==ppass:
+                    print("Logged in as "+User.users[pid][0])
+                    quizzer(pid)
+                else:
+                    print("Invalid Creds")
 
-while True:
-    print("==========================================")
-    print("Enter Option 1> Login 2> Register")
-    lr=int(input())
-    if lr==1:
-        print("Login as Enter Option 1 or 2 \n 1> participant 2> quizerr")
-        opt=int(input())
-        if opt==1:
-            pid=input("enter userid : ")
-            ppass=input("enter pasword : ")
-            if User.users[pid][1]==ppass:
-                print("Logged in as "+User.users[pid][0])
-                participant(pid)
+        elif lr==2:
+            print("Register as Enter Option 1 or 2 \n 1> participant 2> quizerr ")
+            opt=int(input())
+            if opt==1:
+                pn=input("enter name : ")
+                pid=input("enter userid : ")
+                ppass=input("enter pasword : ")
+                pclg=input("enter clg name : ")
+                prolno=input("enter rollno : ")
+                p=Parti(pn,pid,ppass,pclg,prolno)
+                p.save()
+            elif opt==2:
+                pn=input("enter name : ")
+                pid=input("enter userid : ")
+                ppass=input("enter pasword : ")
+                pcomp=input("enter company name : ")
+                p=Quizr(pn,pid,ppass,pcomp)
+                p.save()
+                print(User.users[p.uid])
             else:
-                print("Invalid Creds")
-        elif opt==2:
-            pid=input("enter userid : ")
-            ppass=input("enter pasword : ")
-            if User.users[pid][1]==ppass:
-                print("Logged in as "+User.users[pid][0])
-                quizzer(pid)
-            else:
-                print("Invalid Creds")
+                print("invalid input! ")
 
-    elif lr==2:
-        print("Register as Enter Option 1 or 2 \n 1> participant 2> quizerr ")
-        opt=int(input())
-        if opt==1:
-            pn=input("enter name : ")
-            pid=input("enter userid : ")
-            ppass=input("enter pasword : ")
-            pclg=input("enter clg name : ")
-            prolno=input("enter rollno : ")
-            p=Parti(pn,pid,ppass,pclg,prolno)
-            p.save()
-        elif opt==2:
-            pn=input("enter name : ")
-            pid=input("enter userid : ")
-            ppass=input("enter pasword : ")
-            pcomp=input("enter company name : ")
-            p=Quizr(pn,pid,ppass,pcomp)
-            p.save()
-            print(User.users[p.uid])
         else:
-            print("invalid input! ")
-
-    else:
-        print("invalid input")
-
+            print("invalid input")
+        
+if __name__=='__main__':
+    print("main")
+    #quizapp()
+    q1=Quizr("name","1","123","mrc")
+    p1=Parti("name","2","123","rec","19")
+    p1.save()
+    q1.save()
+    print(User.users)
+    q1.addquiz("t/f","cse")
+    print(Quizr.quizes)
+    participant(p1.uid)
 
 
 
