98f1e1cd152d2041790a4660882c915c81024a7a
Dash <dhanushdpandian@gmail.com>
2024-08-04 22:07:09 +0530

    moved

--- daily/4.08.24daily.py
+++ daily/4.08.24daily.py
@@ -1,3 +1,5 @@
+from typing import List
+#Naive solution
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
         a=[]
@@ -12,3 +14,37 @@ class Solution:
             print(a[i])
             s+=a[i]
         return s % ( (10**9) +7)
+    
+nums=[1,2,3,4]
+# a=[s+i for i in nums for s in a]
+subarray_sums = [sum(nums[start:end]) for start in range(len(nums)) for end in range(start + 1, len(nums)+1)]
+print(subarray_sums)
+
+#One-Liner
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        return sum(sorted([sum(nums[start:end]) for start in range(len(nums)) for end in range(start + 1, len(nums)+1)])[left-1:right]) % (10**9 +7)
+
+
+
+ #Optimized solution   
+def subarrsums(arr,n):
+    a=[]
+    s=0
+    for i in range(n):
+        s+=arr[i]
+        a.append(s)
+        #print(s)
+    ans=[]
+    # ans.append(a[0])
+    for i in range(n):
+        for j in range(i,n):
+            #print(a[i],a[j])
+            if i==0:
+                ans.append(a[j])
+            else:
+                ans.append(a[j]-a[i-1])
+    return ans
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        return sum(sorted(subarrsums(nums,n))[left-1:right]) % (10**9 +7)
