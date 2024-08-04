4fdb8463230892fa6e800f1fe064622a3eaf6bf9
Dash <dhanushdpandian@gmail.com>
2024-08-04 22:06:12 +0530

    optimised subarr sum

--- py/daily/4.08.24daily.py
+++ py/daily/4.08.24daily.py
@@ -1,5 +1,5 @@
 from typing import List
-
+#Naive solution
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
         a=[]
@@ -20,6 +20,31 @@ nums=[1,2,3,4]
 subarray_sums = [sum(nums[start:end]) for start in range(len(nums)) for end in range(start + 1, len(nums)+1)]
 print(subarray_sums)
 
+#One-Liner
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
         return sum(sorted([sum(nums[start:end]) for start in range(len(nums)) for end in range(start + 1, len(nums)+1)])[left-1:right]) % (10**9 +7)
+
+
+
+ #Optimized solution   
+def subarrsums(arr,n):
+    a=[]
+    s=0
+    for i in range(n):
+        s+=arr[i]
+        a.append(s)
+        #print(s)
+    ans=[]
+    # ans.append(a[0])
+    for i in range(n):
+        for j in range(i,n):
+            #print(a[i],a[j])
+            if i==0:
+                ans.append(a[j])
+            else:
+                ans.append(a[j]-a[i-1])
+    return ans
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        return sum(sorted(subarrsums(nums,n))[left-1:right]) % (10**9 +7)
