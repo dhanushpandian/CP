89a0c325c6c328d04d17c73304c43851e69667dd
Dash <dhanushdpandian@gmail.com>
2024-08-05 14:30:24 +0530

    date-f string

--- py/general/stringDateformat.py
+++ py/general/stringDateformat.py
@@ -1,3 +1,13 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        s = date.split() 
+        monthDict = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
+        day = s[0][:-2] # Removes the last 2 elements of the day
+        month = s[1] 
+        year = s[2]
+        if int(day) < 10: # Adds 0 to the front of day if day < 10
+            day = '0' + day
+        return ''.join(f'{year}-{monthDict[month]}-{day}') # Joins it all together. Month is used to draw out the corresponding number from the dict.
 
 class Solution:
     def reformatDate(self, date: str) -> str:
