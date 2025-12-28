d3c995d7f6d6980cd7a45cea2ea354f0d94a8cc8
Dash <dhanushdpandian@gmail.com>
2025-12-28 21:32:38 +0530

    daily2

--- daily/28.12.25daily.py
+++ daily/28.12.25daily.py
@@ -9,3 +9,26 @@ class Solution:
                 c+=1
             print()
         return len(grid)*len(grid[0]) - c
+    
+
+# 1 2 3 4 -1 -2  -3 
+# l     m         r
+#       l    m    r
+#       l  m  r
+
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        c=0
+        for i in range(len(grid)):
+            l=0
+            r=len(grid[i])-1
+            while l<=r:
+                m = (r+l)//2
+                if grid[i][m] < 0:
+                    r = m -1
+                else:
+                    l = m + 1
+            c+= len(grid[i]) - l
+        return c
+                
+
